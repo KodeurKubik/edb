6cdbdf37678516ef068c6fd4142e01621527d5e8
Kubik <84036232+TotoroGaming@users.noreply.github.com>
2022-09-21 20:33:03 +0200

    Create ping.js

---
+++ templates/commands/ping.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, Client } = require("discord.js");
+
+module.exports = {
+    slash: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Ping the bot to know if it works!')
+    ,
+    guilds: "ALL", // Put ALL for a general slash command or put an array of guild id's to create the command in specific guilds
+                // guild: "ALL"
+                // guild: ['Guild Id 1', 'Guild Id 2', '...']
+    /**
+     * @param {Client} client 
+     * @param {import("discord.js").Interaction} interaction 
+     */
+    execute: async (client, interaction) => {
+        // Your code here
+        interaction.reply(`Pong !\nLatency: ${client.ws.ping}\nUptime: <t${client.uptime}:> (<t${client.uptime}:R>)`)
+    }
+}
