fb9f8a692eeb95636f608e510ae1f2eabc057c14
Kubik <84036232+TotoroGaming@users.noreply.github.com>
2022-09-23 08:40:26 +0200

    Create ready.js

---
+++ templates/events/ready.js
@@ -0,0 +1,9 @@
+const { Client } = require("discord.js")
+
+/**
+ * @param {Client} client 
+ */
+module.exports = async (client) => {
+    console.log(`Logged in as @${client.user.tag}`);
+  // Your code here
+}
