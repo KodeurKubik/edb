f6e82487e542d51b886564b0885481350744c77e
Kubik <84036232+TotoroGaming@users.noreply.github.com>
2022-09-23 08:40:12 +0200

    Delete ..js

--- templates/event/..js
+++
@@ -1,8 +0,0 @@
-const { Client } = require("discord.js")
-
-/**
- * @param {Client} client 
- */
-module.exports = async (client) => { // Import all the arguments of this event
-  // Your code here
-}
