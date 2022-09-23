2178ddcce5de85913ca978bcb8e8e8bc1a818de9
Kubik <84036232+TotoroGaming@users.noreply.github.com>
2022-09-23 08:41:43 +0200

    Update index.js

--- templates/init/index.js
+++ templates/init/index.js
@@ -4,9 +4,10 @@ const { Client } = require('discord.js');
 const { readdirSync } = require('fs');
 const client = new Client({ intents });
 const edb = require('./edb/index.js');
+const commands = new Map();
 //================================================\\
 
-client.on('ready', async () => {
+client.once('ready', async () => {
     let cmds = {};
     await readdirSync('./commands').forEach(async path => {
         if (!path.endsWith('.js')) return edb.log.warn(`[!] File "${path}" in /commands don't end with .js | Skipping...`)
@@ -17,14 +18,12 @@ client.on('ready', async () => {
     })
     await Object.keys(cmds).forEach(async guild => {
         let cmd = cmds[guild];
-        if (guild == 'ALL') { client.application.commands.set(cmd); await edb.log.log(`[-] Deployed "${cmd.name}" command in all guilds`); }
-        else {
-            cmd.forEach(async g => {
-                if (!client.guilds.cache.get(guild)) return edb.log.warn(`[!] Commands for guild "${g}" not loaded | Guild not found`);
-                client.guilds.cache.get(guild).commands.set(cmd);
-            })
-            await edb.log.log(`[-] Deployed "${cmd[0].name}" command in ${cmd.length} guilds`);
-        }
+        if (guild == 'ALL') { client.application.commands.set(cmd); }
+        else { if (!client.guilds.cache.get(guild)) return edb.log.warn(`[!] Commands for guild "${g}" not loaded | Guild not found`); client.guilds.cache.get(guild).commands.set(cmd); }
+        await cmd.forEach(c => {
+            edb.log.log(`[-] Deployed "${c.name}" command in ${(guild == 'ALL') ? 'all guilds' : `the "${client.guilds.cache.get(guild).name}" guild` }`);
+            commands.set(c.name, { name: c.name, description: c.description, options: c.options });
+        })
     })
 
     await readdirSync('./events').forEach(async path => {
@@ -32,10 +31,10 @@ client.on('ready', async () => {
         let f = require(`./events/${path}`);
         if (!f || typeof f != 'function') return edb.log.warn(`[!] Event "${path}" don't respect patern of module.exports! See here: https://github.com/TotoroGaming/edb/wiki/Exporting-things#export-an-event`)
         if (path == 'ready.js') f(client)
-        else client.on(path.replace('.js', ''), f);
+        else client.on(path.replace('.js', ''), (...args) => f(client, ...args));
         await edb.log.log(`[-] Loaded "${path}" event`);
     })
 
     if (status != {}) client.user.setActivity(status);
 })
-client.login(token)
+client.login(token);
