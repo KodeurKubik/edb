2a9c525a100162b8f1599518ba6a783ac7ef5fcd
Kubik <84036232+TotoroGaming@users.noreply.github.com>
2022-09-23 08:39:46 +0200

    Create ..js

---
+++ templates/events/..js
@@ -0,0 +1,8 @@
+const { Client } = require("discord.js")
+
+/**
+ * @param {Client} client 
+ */
+module.exports = async (client) => { // Import all the arguments of this event
+  // Your code here
+}
